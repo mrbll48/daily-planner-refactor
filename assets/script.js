388e4de8b5328211cf7a56c71b8b68a63912b7e6
mrbll48 <william.marks87@gmail.com>
2023-04-18 14:37:04 -0700

    finished script.js

--- assets/script.js
+++ assets/script.js
@@ -58,59 +58,60 @@ var times = [
 ]
 
 $(function () {
+  
   // loop to create all of the time blocks for standard business hours
   for (var i = 0; i < times.length; i++){
+    // generates the entire row where the time blocks sit
     var timeBlock = $('<div>');
     timeBlock.addClass('time-block row');
-  
+    timeBlock.attr('id', times[i].id);
+  // generates the box that shows the time
     var timeBox = $('<div>');
     timeBox.addClass('col-2 col-md-1 hour text-center py-3');
     timeBox.text(times[i].hour + times[i].ampm);
-    
+    // generates the text area where notes can be typed
     var textArea = $('<textarea>');
     textArea.addClass('col-8 col-md-10 description textarea');
-    textArea.attr('rows', 3)
-    textArea.text('Input message here');
-  
+    textArea.attr('rows', 3);
+    textArea.attr('placeholder', 'Input message here');
+    textArea.text(localStorage.getItem(times[i].id));
+  // generates the styling for the submit button
     var btnHolder = $('<i>');
-    btnHolder.addClass('fas fa-save')
-    btnHolder.attr('aria-hidden', true)
-  
+    btnHolder.addClass('fas fa-save');
+    btnHolder.attr('aria-hidden', true);
+  // generates the submit button
     var submitBtn = $('<button>'); 
     submitBtn.addClass('btn saveBtn col-2 col-md-1');
-    submitBtn.attr('aria-label', 'save')
+    submitBtn.attr('aria-label', 'save');
+    submitBtn.click(localStore);
+
+    var currentTime= dayjs().hour();
     
-    $('.px-5').append(timeBlock)
-    $(timeBlock).append(timeBox, textArea, submitBtn)
+    $('.px-5').append(timeBlock);
+    $(timeBlock).append(timeBox, textArea, submitBtn);
     $(submitBtn).append(btnHolder);
   // conditional to check the current time with the time on the timeblock. Applies color based on what time of day it is. 
-    if (times[i].hr < dayjs().hour()) {
-      timeBlock.addClass('past')
-    }  else if (times[i].hr > dayjs().hour()) {
-      timeBlock.addClass('future')
+    if (times[i].hr < currentTime) {
+      timeBlock.addClass('past');
+    }  else if (times[i].hr > currentTime) {
+      timeBlock.addClass('future');
     } else {
-      timeBlock.addClass('present')
+      timeBlock.addClass('present');
     }
   }
-
+// function designed to set the text from textArea into localstorage on clicking the submit button
+function localStore() {
+  var id = $(this).parent().attr('id');
+  var textContent = $(this).siblings('.description').val();
+  localStorage.setItem(id, textContent);
+}
   
-  
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
   displayDateTime();
   // function to display the date and time at the bottom of the header
   function displayDateTime() {
     var currentDayEl = $('#currentDay');
     var currentTimeEl = $('#currentTime');
-    var currentDay = dayjs().format('dddd MMMM D, YYYY')
+    var currentDay = dayjs().format('dddd MMMM D, YYYY');
     var currentTime = dayjs().format('h[:]mm a');
     currentDayEl.text(currentDay);
     currentTimeEl.text(currentTime);
