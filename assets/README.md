1085828fe8cf1915f0e0f88a47c075cbb31d76cf
mrbll48 <william.marks87@gmail.com>
2023-04-17 18:24:04 -0700

    updated script.js. hour blocks dynamically created and color coded based on time of day

--- assets/README.md
+++ assets/README.md
@@ -17,15 +17,15 @@ SO THAT I can manage my time effectively
 ## Acceptance Criteria
 
 ```md
-GIVEN I am using a daily planner to create a schedule
+<!-- GIVEN I am using a daily planner to create a schedule
 WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
+THEN the current day is displayed at the top of the calendar  -->
+<!-- WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours -->
+<!-- WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future -->
+<!-- WHEN I click into a timeblock
+THEN I can enter an event -->
 WHEN I click the save button for that timeblock
 THEN the text for that event is saved in local storage
 WHEN I refresh the page
@@ -35,7 +35,7 @@ THEN the saved events persist
 The following animation demonstrates the application functionality:
 
 <!-- @TODO: create ticket to review/update image) -->
-![A user clicks on slots on the color-coded calendar and edits the events.](./Assets/05-third-party-apis-homework-demo.gif)
+![A user clicks on slots on the color-coded calendar and edits the events.](./assets/05-third-party-apis-homework-demo.gif)
 
 ## Grading Requirements
 
--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,120 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+// array used to generate all of the time blocks, enter the time into each timeblock, and apply past, present, future tags
+var times = [
+  {
+    hour: 9,
+    ampm: ' AM',
+    id: 'hour-9',
+    hr: 9
+  },
+  {
+    hour: 10,
+    ampm: ' AM',
+    id: 'hour-10',
+    hr: 10
+  },
+  {
+    hour: 11,
+    ampm: ' AM',
+    id: 'hour-11',
+    hr: 11
+  },
+  {
+    hour: 12,
+    ampm: ' PM',
+    id: 'hour-12',
+    hr: 12
+  },
+  {
+    hour: 1,
+    ampm: ' PM',
+    id: 'hour-1',
+    hr: 13
+  },
+  {
+    hour: 2,
+    ampm: ' PM',
+    id: 'hour-2',
+    hr: 14
+  },
+  {
+    hour: 3,
+    ampm: ' PM',
+    id: 'hour-3',
+    hr: 15
+  },
+  {
+    hour: 4,
+    ampm: ' PM',
+    id: 'hour-4',
+    hr: 16
+  },
+  {
+    hour: 5,
+    ampm: ' PM',
+    id: 'hour-5',
+    hr: 17
+
+  },
+]
+
 $(function () {
+  // loop to create all of the time blocks for standard business hours
+  for (var i = 0; i < times.length; i++){
+    var timeBlock = $('<div>');
+    timeBlock.addClass('time-block row');
+  
+    var timeBox = $('<div>');
+    timeBox.addClass('col-2 col-md-1 hour text-center py-3');
+    timeBox.text(times[i].hour + times[i].ampm);
+    
+    var textArea = $('<textarea>');
+    textArea.addClass('col-8 col-md-10 description textarea');
+    textArea.attr('rows', 3)
+    textArea.text('Input message here');
+  
+    var btnHolder = $('<i>');
+    btnHolder.addClass('fas fa-save')
+    btnHolder.attr('aria-hidden', true)
+  
+    var submitBtn = $('<button>'); 
+    submitBtn.addClass('btn saveBtn col-2 col-md-1');
+    submitBtn.attr('aria-label', 'save')
+    
+    $('.px-5').append(timeBlock)
+    $(timeBlock).append(timeBox, textArea, submitBtn)
+    $(submitBtn).append(btnHolder);
+  // conditional to check the current time with the time on the timeblock. Applies color based on what time of day it is. 
+    if (times[i].hr < dayjs().hour()) {
+      timeBlock.addClass('past')
+    }  else if (times[i].hr > dayjs().hour()) {
+      timeBlock.addClass('future')
+    } else {
+      timeBlock.addClass('present')
+    }
+  }
+
+  
+  
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
   // function? How can DOM traversal be used to get the "hour-x" id of the
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
+
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+  displayDateTime();
+  // function to display the date and time at the bottom of the header
+  function displayDateTime() {
+    var currentDayEl = $('#currentDay');
+    var currentTimeEl = $('#currentTime');
+    var currentDay = dayjs().format('dddd MMMM D, YYYY')
+    var currentTime = dayjs().format('h[:]mm a');
+    currentDayEl.text(currentDay);
+    currentTimeEl.text(currentTime);
+  }
+  // refreshes the time every second to keep the time up to date
+   setInterval(displayDateTime, 1000);
 });
